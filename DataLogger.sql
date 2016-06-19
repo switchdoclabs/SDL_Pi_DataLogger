73bf75d6bace70dc35e723d63a747862aecb7f0c
John Shovic <jshovic@switchdoc.com>
2016-06-19 23:39:47 +0000

    OurWeather - Updated MySQL Structure

--- DataLogger.sql
+++ DataLogger.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Jun 12, 2016 at 12:46 AM
+-- Generation Time: Jun 19, 2016 at 11:37 PM
 -- Server version: 5.5.44-0+deb8u1
 -- PHP Version: 5.6.20-0+deb8u1
 
@@ -28,7 +28,6 @@ USE `DataLogger`;
 -- Table structure for table `ADS1115Table`
 --
 
-DROP TABLE IF EXISTS `ADS1115Table`;
 CREATE TABLE IF NOT EXISTS `ADS1115Table` (
 `id` int(11) NOT NULL,
   `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
@@ -49,7 +48,6 @@ CREATE TABLE IF NOT EXISTS `ADS1115Table` (
 -- Table structure for table `INA3221Table`
 --
 
-DROP TABLE IF EXISTS `INA3221Table`;
 CREATE TABLE IF NOT EXISTS `INA3221Table` (
 `id` int(11) NOT NULL,
   `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
@@ -61,6 +59,38 @@ CREATE TABLE IF NOT EXISTS `INA3221Table` (
   `channel3_load_voltage` float NOT NULL,
   `channel3_current` float NOT NULL
 ) ENGINE=InnoDB AUTO_INCREMENT=899 DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `OURWEATHERTable`
+--
+
+CREATE TABLE IF NOT EXISTS `OURWEATHERTable` (
+`ID` int(20) NOT NULL,
+  `TimeStamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `deviceid` int(11) NOT NULL,
+  `Outdoor_Temperature` float NOT NULL,
+  `Outdoor_Humidity` float NOT NULL,
+  `Indoor_Temperature` float NOT NULL,
+  `Barometric_Pressure` float NOT NULL,
+  `Altitude` float NOT NULL,
+  `Current_Wind_Speed` float NOT NULL,
+  `Current_Wind_Gust` float NOT NULL,
+  `Current_Wind_Direction` float NOT NULL,
+  `Rain_Total` float NOT NULL,
+  `Wind_Speed_Minimum` float NOT NULL,
+  `Wind_Speed_Maximum` float NOT NULL,
+  `Wind_Gust_Minimum` float NOT NULL,
+  `Wind_Gust_Maximum` float NOT NULL,
+  `Wind_Direction_Minimum` float NOT NULL,
+  `Wind_Direction_Maximum` float NOT NULL,
+  `Display_English_Metrice` int(11) NOT NULL,
+  `OurWeather_DateTime` varchar(30) NOT NULL,
+  `OurWeather_Station_Name` varchar(30) NOT NULL,
+  `Current_Air_Quality_Sensor` int(11) NOT NULL,
+  `Current_Air_Quality_Qualitative` int(11) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=87 DEFAULT CHARSET=latin1 COMMENT='OurWeather FullDataString';
 
 --
 -- Indexes for dumped tables
@@ -79,6 +109,12 @@ ALTER TABLE `INA3221Table`
  ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `OURWEATHERTable`
+--
+ALTER TABLE `OURWEATHERTable`
+ ADD PRIMARY KEY (`ID`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -92,6 +128,11 @@ MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
 --
 ALTER TABLE `INA3221Table`
 MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=899;
+--
+-- AUTO_INCREMENT for table `OURWEATHERTable`
+--
+ALTER TABLE `OURWEATHERTable`
+MODIFY `ID` int(20) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=87;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
